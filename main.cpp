d3e1e8cdd33d0c8ba9adb082d0a451101968d29e
RoblKyogre <42159204+RoblKyogre@users.noreply.github.com>
2020-05-16 20:46:17 -0700

    Implement Pause & Reset

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <GLFW/glfw3.h>
 #include "include/box2d/box2d.h"
 #include <vector>
+#include <math.h>
 
 using namespace std;
+
+string key_action = "";
 
 void error_callback(int error, const char* description)
 {
@@ -14,12 +17,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+        key_action = "pause";
+    if (key == GLFW_KEY_ENTER && action == GLFW_PRESS)
+        key_action = "reset";
 }
 
-int draw(vector<double> &pendulumA, vector<double> &pendulumB)
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+
+}
+
+int draw()
 {
 
     GLFWwindow* window;
+
+    /* Program-Controlled Variables */
 
     int width = 1280;
     int height = 720;
@@ -29,7 +43,27 @@ int draw(vector<double> &pendulumA, vector<double> &pendulumB)
     double previous_time = -1.0;
     double time_delta = -1.0; //This will be used in the physics "time step"
 
-    bool is_sim_running = false;
+    int velocityIterations = 6;
+    int positionIterations = 2;
+    
+    vector<double> PenA_Render(2,-1);
+    vector<double> PenB_Render(2,-1);
+    vector<double> center_render(2,-1);
+
+    /* User-Controlled Variables */
+
+    bool is_sim_paused = true;
+
+    vector<float> PenA_StartVec(2,0);
+    vector<float> PenB_StartVec(2,0);
+
+    PenA_StartVec[0] = 5.0f;
+    PenA_StartVec[1] = 0.0f;
+
+    PenB_StartVec[0] = 10.0f;
+    PenB_StartVec[1] = 0.0f;
+
+    double scale_factor = 50;
 
     /* Initialize GLFW */
     glfwSetErrorCallback(error_callback);
@@ -43,45 +77,139 @@ int draw(vector<double> &pendulumA, vector<double> &pendulumB)
     }
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     glfwSwapInterval(1);
 
     /* Initialize Physics */
-    b2Vec2 gravity(0.0f, -9.8f);
+    b2Vec2 gravity(0.0f, -10.0f);
     b2World world(gravity);
 
+    b2BodyDef centerDef;
+    centerDef.position.Set(0.0f, 0.0f);
+    b2Body* center = world.CreateBody(&centerDef);
 
+    b2BodyDef PenA_Def;
+    PenA_Def.type = b2_dynamicBody;
+    PenA_Def.position.Set(PenA_StartVec[0], PenA_StartVec[1]);
+    b2Body* PenA = world.CreateBody(&PenA_Def);
 
+    b2BodyDef PenB_Def;
+    PenB_Def.type = b2_dynamicBody;
+    PenB_Def.position.Set(PenB_StartVec[0],PenB_StartVec[1]);
+    b2Body* PenB = world.CreateBody(&PenB_Def);
+
+    b2CircleShape PenShape;
+    PenShape.m_radius= 0.5f;
+
+    b2FixtureDef PenFixtureDef;
+    PenFixtureDef.shape = &PenShape;
+    PenFixtureDef.density = 1.0f;
+    PenFixtureDef.friction = 0.0f;
+    PenFixtureDef.restitution = 1.0f;
+
+    PenA->CreateFixture(&PenFixtureDef);
+    PenB->CreateFixture(&PenFixtureDef);
+
+    b2RevoluteJointDef JointADef;
+    JointADef.bodyA = center;
+    JointADef.bodyB = PenA;
+    JointADef.collideConnected = false;
+    JointADef.localAnchorA.Set(0,0);
+    JointADef.localAnchorB.Set(
+        0 - PenA_StartVec[0],
+        0 - PenA_StartVec[1]
+    );
+    b2Joint* JointA = (b2RevoluteJoint*)world.CreateJoint(&JointADef);
+    
+    b2RevoluteJointDef JointBDef;
+    JointBDef.bodyA = PenA;
+    JointBDef.bodyB = PenB;
+    JointBDef.collideConnected = false;
+    JointBDef.localAnchorA.Set(0,0);
+    JointBDef.localAnchorB.Set(
+        (0 - PenB_StartVec[0]) + PenA_StartVec[0],
+        0 - PenB_StartVec[1] + PenA_StartVec[1]
+    );
+    b2Joint* JointB = (b2RevoluteJoint*)world.CreateJoint(&JointBDef);
+    
+   
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        
         
         /* Run calculations */
         previous_time = program_time;
         program_time = glfwGetTime();
         time_delta = program_time - previous_time;
-
-        /* Run physics */
-        if (is_sim_running)
-        {
-            cout<<"Physics is running";
-        }
-        else
-        {
-            cout<<"Physics is not running";
-        }
         
         /* Render here */
+        b2Vec2 center_pos = center->GetPosition();
+        b2Vec2 PenA_Pos = PenA->GetPosition();
+        b2Vec2 PenB_Pos = PenB->GetPosition();
+
         glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / height;
+        ratio = width/height;
+
+        center_render[0] = center_pos.x * scale_factor / width;
+        center_render[1] = center_pos.y * scale_factor / height;
+
+        PenA_Render[0] = PenA_Pos.x * scale_factor / width;
+        PenA_Render[1] = PenA_Pos.y * scale_factor / height;
+
+        PenB_Render[0] = PenB_Pos.x * scale_factor / width;
+        PenB_Render[1] = PenB_Pos.y * scale_factor / height;
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        glPointSize(10.0f);
+
+        glColor3d(1.0,0.0,0.0);
+
+        glBegin(GL_POINTS);
+            glVertex2f(center_render[0],center_render[1]);
+            glVertex2f(PenA_Render[0], PenA_Render[1]);
+            glVertex2f(PenB_Render[0], PenB_Render[1]);
+        glEnd();
+
+        glBegin(GL_LINE_STRIP);
+            glVertex2f(center_render[0],center_render[1]);
+            glVertex2f(PenA_Render[0], PenA_Render[1]);
+            glVertex2f(PenB_Render[0], PenB_Render[1]);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
+        /* Run physics */
+        if (!is_sim_paused)
+        {
+            world.Step(time_delta, velocityIterations, positionIterations);
+        }
+
         /* Poll for and process events */
         glfwPollEvents();
+        if (key_action == "pause")
+        {
+            is_sim_paused = !is_sim_paused;
+        }
+        if (key_action == "reset")
+        {
+            PenA->SetTransform(b2Vec2(PenA_StartVec[0],
+            PenA_StartVec[1]),0.0f);
+            PenA->SetLinearVelocity(b2Vec2(0,0));
+
+            PenB->SetTransform(b2Vec2(PenB_StartVec[0],
+            PenB_StartVec[1]),0.0f);
+            PenB->SetLinearVelocity(b2Vec2(0,0));
+
+            is_sim_paused = true;
+        }
+        key_action = "";
+
+
+
     }
 
     glfwTerminate();
@@ -94,13 +222,10 @@ int main()
 {
 
     int draw_return = 0;
+    cout<<"<Enter>: Reset"<<endl<<"<Space>: Pause"<<endl;
 
-    vector<double> pendulumA(2,0); // These will store X and Y coordinates as meters, not pixels
-    vector<double> pendulumB(2,0);
+    draw_return = draw();
 
-    draw_return = draw(pendulumA, pendulumB);
-
-    cout<<"Return Value: "<<draw_return<<endl;
     return draw_return;
 
 }
